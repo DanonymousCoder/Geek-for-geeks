3041671ad719f3662e1da9fba6b77785fa601584
Ridwan <aladebaruwa@gmail.com>
2023-06-26 19:33:40 +0100

    add

--- index.js
+++ index.js
@@ -33,4 +33,4 @@ const greeting = "Hello!, My name is " + fullName + " and I'm " + age + " years 
 console.log(greeting);
 let test = "This is a test";
 window.alert(test);
-window.prompt(test);
+window.prompt("input a number");
