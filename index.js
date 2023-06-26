8b2560239dcd39addb5bcb6adf7df55934c8dc8c
Ridwan <aladebaruwa@gmail.com>
2023-06-26 11:26:56 +0100

    add

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function myg() {
     document.getElementById('ja') .innerHTML = 'External JavaScript'
 }
 let a, b, c;
-a= 1;
-b= 2;
+a= '1';
+b= '2';
 c= a+b;
-alert('The values of c is "+ c +".');
+alert('The values of c is " c ".');
